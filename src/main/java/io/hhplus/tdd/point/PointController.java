790fec85009b820e353a734474885f7ef9997123
ksh <googoojoa@gmail.com>
2025-07-09 14:15:28 +0900

    feat : 특정 유저 포인트 조회 기능

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.UserPointTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,11 @@ import java.util.List;
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final UserPointTable userPointTable;
+
+    public PointController(UserPointTable userPointTable) {
+        this.userPointTable = userPointTable;
+    }
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
@@ -19,7 +25,7 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        return userPointTable.selectById(id);
     }
 
     /**
