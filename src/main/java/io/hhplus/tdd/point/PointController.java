78c13f6f77bf2beb7270b84d3187e154a2d44bb4
ksh <googoojoa@gmail.com>
2025-07-10 11:03:41 +0900

    feat : 포인트 충전 시 사용자별 동시 요청에 대한 처리 로직 추가

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -7,14 +7,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping("/point")
 public class PointController {
-
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
+    // ConcurrentHashMap - 락을 보관할 공간, 여러 쓰레드가 동시에 접근해도 안전하게 동작 가능
+    // 사용자별 ID(Long)으로 Object(락객체) 저장
+    private final ConcurrentHashMap<Long, Object> userLocks = new ConcurrentHashMap<>();
 
     public PointController(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
         this.userPointTable = userPointTable;
@@ -49,22 +52,26 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        //충전할 포인트가 음수로 입력되었을 때
-        if (amount <= 0) {
-            throw new IllegalArgumentException("0보다 큰 금액만 충전할 수 있습니다.");
+        // 동시성 제어를 위한 lock(한 사용자의 id에 여러번 포인트 충전)
+        Object lock = userLocks.computeIfAbsent(id, k -> new Object());
+        // 동기화 코드
+        synchronized (lock) {
+            if (amount <= 0) {  //충전할 포인트가 음수로 입력되었을 때
+                throw new IllegalArgumentException("0보다 큰 금액만 충전할 수 있습니다.");
+            }
+            UserPoint current = userPointTable.selectById(id);
+            long now = System.currentTimeMillis();
+            long maxPoint = 50000L;
+            long updatedAmount = current.point() + amount;
+            // 최대 충전 금액 제한
+            if(updatedAmount > maxPoint){
+                throw new IllegalArgumentException("최대 충전 금액은 " +maxPoint+ "원 입니다.");
+            }
+            // 포인트 충전 시 이력
+            pointHistoryTable.insert(id, amount, TransactionType.CHARGE, now);
+            // 업데이트된 유저 포인트 반환
+            return userPointTable.insertOrUpdate(id, updatedAmount);
         }
-        UserPoint current = userPointTable.selectById(id);
-        long now = System.currentTimeMillis();
-        long maxPoint = 50000L;
-        long updatedAmount = current.point() + amount;
-        // 최대 충전 금액 제한
-        if(updatedAmount > maxPoint){
-            throw new IllegalArgumentException("최대 충전 금액은 " +maxPoint+ "원 입니다.");
-        }
-        // 포인트 충전 시 이력
-        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, now);
-        // 업데이트된 유저 포인트 반환
-        return userPointTable.insertOrUpdate(id, updatedAmount);
     }
 
     /**
